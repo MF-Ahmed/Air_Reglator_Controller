54977793ff3c69c8225129f5d6150793666bad33
MF-Ahmed <itsfarhan@outlook.com>
2022-10-21 13:16:11 +0200

    Updated paths
    from chapter 4,5

--- main.py
+++ main.py
@@ -28,16 +28,12 @@ class WorkerSignals(QObject):
     Defines the signals available from a running worker thread.
 
     Supported signals are:
-
     finished
         No data
-
     error
         `tuple` (exctype, value, traceback.format_exc() )
-
     result
         `object` data returned from processing, anything
-
     progress
         `int` indicating % progress
 
